ef3fa5e6119fe7dab486adf668bf8d67445f7cbd
Andre Wong <wongandre98@gmail.com>
2020-01-08 02:20:08 +0800

    updated package.json and added files

--- server.js
+++ server.js
@@ -10,7 +10,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex')
 
