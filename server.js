c24ace9997f49195203147974b2a96138b115903
Andre Wong <wongandre98@gmail.com>
2020-01-08 02:25:14 +0800

    updated bcrypt-nodejs

--- server.js
+++ server.js
@@ -10,7 +10,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex')
 
